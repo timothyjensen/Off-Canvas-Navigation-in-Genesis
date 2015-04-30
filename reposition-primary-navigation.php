8d91c9a9473750be6ff16a2c59a1ef1dcc6e75d3
Calvin Makes <hello@calvinkoepke.com>
2015-04-29 20:04:18 -0600

    Update reposition-primary-navigation.php

--- reposition-primary-navigation.php
+++ reposition-primary-navigation.php
@@ -1,6 +1,9 @@
 <?php
 //* Do NOT include the opening PHP tag
 
-//* Reposition the Primary navigation at the top of the DOM.
+/** Reposition the Primary navigation at the top of the DOM.
+  * @author Calvin Makes
+  * @link http://www.calvinmakes.com/add-a-mobile-friendly-off-canvas-menu-in-genesis
+ */
 remove_action( 'genesis_after_header', 'genesis_do_nav' );
 add_action( 'genesis_before', 'genesis_do_nav', 1 );
