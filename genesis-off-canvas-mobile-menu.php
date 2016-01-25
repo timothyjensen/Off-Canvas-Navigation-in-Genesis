6b0314aecb69ff7841dd21a6c8f57ff62447080e
timothyjensen <tim@timjensen.us>
2016-01-25 12:11:22 -0500

    Some tweaks to make this a working plugin
    Tested only with Genesis sample child theme.

--- genesis-off-canvas-mobile-menu.php
+++ genesis-off-canvas-mobile-menu.php
@@ -18,25 +18,47 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 /**
+	* @author Brad Dalton
+	* @link http://wpsites.net/web-design/adding-additional-nav-menus-in-genesis/
+*/
+
+add_action( 'init', 'register_additional_menu' );
+add_action( 'genesis_before', 'add_third_nav_genesis' , 4); 
+
+function register_additional_menu() {
+  	register_nav_menu( 'mobile-menu' ,__( 'Mobile Menu' ));
+}
+
+function add_third_nav_genesis() {
+
+	wp_nav_menu( array( 
+		'theme_location' => 'mobile-menu', 
+		'container_class' => 'nav-mobile',
+		'container'       => 'nav',
+		'menu_class'     => 'genesis-mobile-menu',
+		'depth'           => 1
+	) ); 
+}
+
+
+/**
 	* @author Calvin Koepke (@cjkoepke)
 	* @link http://www.calvinkoepke.com/add-a-mobile-friendly-off-canvas-menu-in-genesis/
 	* @version 1.1
 	* @license Free to use as you wish. Please share if you appreciate, and link to the post above.
 */
 
-//* Reposition the Primary navigation at the top of the DOM.
-remove_action( 'genesis_after_header', 'genesis_do_nav' );
-add_action( 'genesis_before', 'genesis_do_nav', 1 );
-
 //* Add the menu to the .site-header, but hooking right before the genesis_header_markup_close action.
 add_action( 'genesis_header', 'ck_menu_button', 14 );
 function ck_menu_button() {
 	
-	//* Make sure primary menu is active
-	if ( has_nav_menu( "primary" ) ) {
+	//* Make sure mobile-menu is active
+	if ( has_nav_menu( "mobile-menu" ) ) {
 
 		echo '<a alt="Toggle Menu" href="#" class="menu-btn right small">' . __( 'Menu', 'CHILD_THEME_TEXT_DOMAIN' ) . '<span class="dashicons dashicons-menu"></span></a>';
 
+	} else {
+		echo '<h2 class="menu-btn right small">Set mobile menu location in Menu settings</h2>';
 	}
   
 }
@@ -48,7 +70,7 @@ function ck_close_btn($menu, $args) {
   
 	$extras = '<a href="#" class="close-btn"><em>' . __( 'Close Navigation', 'CHILD_THEME_TEXT_DOMAIN' ) . '</em> <span class="dashicons dashicons-no"></span></a>';
 
-	if ( $args->theme_location == "primary" ) {
+	if ( $args->theme_location == "mobile-menu" ) {
 
 		return $extras . $menu;
 
@@ -72,7 +94,7 @@ function ck_site_overlay() {
 add_action( 'wp_enqueue_scripts', 'ck_load_menu' );
 function ck_load_menu() {
   
- 	wp_enqueue_script( 'ck-menu', get_stylesheet_directory_uri() . '/js/offcanvas-nav.js', array( 'jquery' ), 1.0 );
+   	wp_enqueue_script( 'ck-menu', plugins_url( '/js/offcanvas-nav.js', __FILE__ ), array( 'jquery' ), 1.0 );
 	wp_enqueue_style('ck-menu-style', plugins_url( '/css/offcanvasmenu-style.css', __FILE__ ) );
 	wp_enqueue_style( 'dashicons' );
 }
