6aed3b46602d1f69ba5aec7c4b1f665d9abad6b6
timothyjensen <tim@timjensen.us>
2016-01-25 13:08:44 -0500

    Some tweaks to make this a working plugin

--- genesis-off-canvas-mobile-menu.php
+++ genesis-off-canvas-mobile-menu.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Genesis Off Canvas Mobile Menu
  * Plugin URI:        http://www.timjensen.us
- * Description:       Add an off-canvas mobile menu for your Genesis child theme.
+ * Description:       Add an off-canvas mobile menu for your Genesis child theme. Activate the plugin, then select the mobile menu location in Appearance -> Menus -> Menu Settings.
  * Version:           1.0.0
  * Author:            Tim Jensen
  * Author URI:        http://www.timjensen.us
@@ -55,12 +55,9 @@ function ck_menu_button() {
 	//* Make sure mobile-menu is active
 	if ( has_nav_menu( "mobile-menu" ) ) {
 
-		echo '<a alt="Toggle Menu" href="#" class="menu-btn right small">' . __( 'Menu', 'CHILD_THEME_TEXT_DOMAIN' ) . '<span class="dashicons dashicons-menu"></span></a>';
+		echo '<a alt="Toggle Menu" href="#" class="menu-btn right small">' . '<span class="dashicons dashicons-menu"></span></a>';
 
-	} else {
-		echo '<h2 class="menu-btn right small">Set mobile menu location in Menu settings</h2>';
-	}
-  
+	}   
 }
 
 //* Add the "Close Navigation" text to the Primary menu output.
@@ -93,8 +90,8 @@ function ck_site_overlay() {
 //* Include our offcanvas-nav.js file in the theme
 add_action( 'wp_enqueue_scripts', 'ck_load_menu' );
 function ck_load_menu() {
-  
    	wp_enqueue_script( 'ck-menu', plugins_url( '/js/offcanvas-nav.js', __FILE__ ), array( 'jquery' ), 1.0 );
 	wp_enqueue_style('ck-menu-style', plugins_url( '/css/offcanvasmenu-style.css', __FILE__ ) );
 	wp_enqueue_style( 'dashicons' );
 }
+
